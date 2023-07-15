93d84cf76a51c56b679e6958253285177d9339c7
amoonra <dvoeglazov@duck.com>
2023-07-15 17:55:28 +0500

    feat: sdf

--- src/store.ts
+++ src/store.ts
@@ -10,7 +10,7 @@ const items = ["Позавтракать", "Сходить в магазин", "
 
 const DEFAULT_STATE = { items, done: {}, text: "" };
 
-// actions-----=-
+// actions-----=-11
 export const addItem = (text: string) => ({ type: "ADD_ITEM", text } as const);
 export const setText = (text: string) => ({ type: "SET_TEXT", text } as const);
 export const setDone = (index: number, done: boolean) =>
